2d6a4265f63da6c41f05e0d6a00e7425aae7fd60
Hitesh Kumar <hiteshkumar@Hiteshs-MacBook-Air.local>
2026-09-02 09:23:17 +0530

    new

--- node js/eventEmitter.js
+++ node js/eventEmitter.js
@@ -2,11 +2,11 @@ const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
 
 // Register an event listener for the 'greet' event
-myEmitter.on('greet', (name) => {
+myEmitter.on('greet', (name) => {//event listener function
     console.log(`Hello, ${name}!`);
 });
 myEmitter.on('exit', () => {
     console.log("application closed");
 });
-myEmitter.emit('greet', 'Alice'); // Emit the 'greet' event
+myEmitter.emit('greet', 'Alice'); // Emit the 'greet' event triggering the event listener
 myEmitter.emit('exit');
