9860111ee507f195b3c606323c445c6534fcff99
hiteshkumar-gif <hiteshjee2400@gmail.com>
2026-09-16 08:59:13 +0530

    new

--- FS_MODULE/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-fs.unlink('sample.txt',(err)=>{
-//     if(err){
-//         console.log('error deleting file:',err);
-//     }
-//     else{
-//         console.log('file delted succesfully');
-//     }
-// })
