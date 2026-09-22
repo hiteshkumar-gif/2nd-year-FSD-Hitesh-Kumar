0455dac6a32dca2389636025b4665d7b9bab80db
hiteshkumar-gif <hiteshjee2400@gmail.com>
2026-09-22 11:29:20 +0530

    new

--- ASSIGNMENT/ass1_quiz.js
+++ ASSIGNMENT/ass1_quiz.js
@@ -1,105 +1,103 @@
-
 let quizForm = document.getElementById("quizForm");
 
 if (quizForm) {
-
     quizForm.addEventListener("submit", function(event) {
-
         event.preventDefault();
-
         window.location.href = "quiz.html";
-
     });
-
 }
 
 let currentQuestion = 0;
-
 let questions = document.querySelectorAll(".question");
-
 let nextBtn = document.getElementById("nextBtn");
-
 let prevBtn = document.getElementById("prevBtn");
 
+let questionTimer = null; 
+// Target layout containers
+let timerDisplay = document.getElementById("timerDisplay"); 
+let timerBox = document.getElementById("timerBox");
 
-// Run this code only if questions exist
 if (questions.length > 0) {
-
     showQuestion(currentQuestion);
-
 }
 
 function showQuestion(index) {
-
-    // Hide all questions
+    
     questions.forEach(function(question) {
-
         question.style.display = "none";
-
     });
 
-
-    // Show current question
+   
     questions[index].style.display = "block";
 
-
-    // Hide Previous button on Question 1
+   
     if (index === 0) {
-
-        prevBtn.style.display = "none";
-
+        if (prevBtn) prevBtn.style.display = "none";
     } else {
-
-        prevBtn.style.display = "inline-block";
-
+        if (prevBtn) prevBtn.style.display = "inline-block";
     }
 
-
-    // Change Next to Submit on last question
+  
     if (index === questions.length - 1) {
-
-        nextBtn.innerText = "Submit Quiz";
-
+        if (nextBtn) nextBtn.innerText = "Submit Quiz";
     } else {
-
-        nextBtn.innerText = "Next";
-
+        if (nextBtn) nextBtn.innerText = "Next";
     }
 
+   
+    startQuestionCountdown();
 }
 
+function startQuestionCountdown() {
+    clearInterval(questionTimer);
+
+    let timeLeft = 30; 
+    if (timerBox) {
+        timerBox.classList.remove("critical");
+    }
+    if (timerDisplay) {
+        timerDisplay.innerText = `Time Left: ${timeLeft}s`;
+    }
+
+    questionTimer = setInterval(function() {
+        timeLeft--; 
+
+        if (timerDisplay) {
+            timerDisplay.innerText = `Time Left: ${timeLeft}s`;
+        }
+
+        if (timeLeft <= 5 && timerBox) {
+            timerBox.classList.add("critical");
+        }
+
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer); 
+
+            alert("Time is up for this question! Moving to the next one.");
+            
+            nextQuestion(); 
+        }
+    }, 1000); 
+}
 
 function nextQuestion() {
-
-    // If last question
     if (currentQuestion === questions.length - 1) {
-
         submitQuiz();
-
         return;
-
     }
 
     currentQuestion++;
-
     showQuestion(currentQuestion);
-
 }
 
 function previousQuestion() {
-
     if (currentQuestion > 0) {
-
         currentQuestion--;
-
         showQuestion(currentQuestion);
-
     }
-
 }
 
 function submitQuiz() {
-
+    clearInterval(questionTimer);
     alert("Quiz Submitted Successfully!");
-
 }
